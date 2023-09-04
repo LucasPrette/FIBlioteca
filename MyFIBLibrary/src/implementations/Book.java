4777b8ba1a12e83f31351f8cee4c0c8a57f8b40a
LucasPrette <lucassprette@hotmail.com>
2023-09-04 12:03:21 -0300

    Testing

--- MyFIBLibrary/src/implementations/Book.java
+++ MyFIBLibrary/src/implementations/Book.java
@@ -17,46 +17,37 @@ public class Book {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
-    };
+    }
 
     public int getIsbn() {
         return this.isbn;
-    };
+    }
 
     public String getAuthor() {
         return this.author;
-    };
+    }
 
     public String getTitle() {
         return this.title;
-    };
+    }
 
 
     public void setIsbn(int isbn) {
         this.isbn = isbn;
-    };
+    }
 
     public void setAuthor(String author) {
         this.author = author;
-    };
+    }
 
     public void setTitle(String title) {
         this.title = title;
-    };
+    }
 
     public void showBook() {
         System.out.println("ISBN: " + this.isbn);
         System.out.println("Title: " + this.title);
         System.out.println("Author: " + this.author);
-    };
-
-    public void showList() {
-        for (int i = 0; i< 100; i++) {
-            System.out.println("EU odeio o git e o github");
-            // it didnt work
-        }
     }
 
-    // is it working or not ?
-
-};
+}
