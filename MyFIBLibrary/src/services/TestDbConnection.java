003b0fbe2a31b9dbfb33e05632a39d35ce324011
LucasPrette <lucassprette@hotmail.com>
2023-09-16 17:33:00 -0300

    Fix: try outside method

--- MyFIBLibrary/src/services/TestDbConnection.java
+++ MyFIBLibrary/src/services/TestDbConnection.java
@@ -3,18 +3,25 @@ package services;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TestDbConnection {
     String url = "jdbc:sqlserver://localhost:1433;databaseName=fib;encrypt=false;";
     String user = "aluno";
     String password = "123";
 
-    try {
-        Connection connection = DriverManager.getConnection(url, user, password);
-        System.out.println("Connected!!");
-        connection.close();
+    public void testConnection () {
 
-    } catch (SQLException e) {
-        e.printStackTrace();
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected!!");
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Connection error!!" + e.getMessage());
+        }
+
     }
+
 }
